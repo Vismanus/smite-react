d30b0e7c6276eac050d60f76648b65f395cad17e
Vismanus <seanfmcd01@gmail.com>
2018-05-03 14:06:41 +0100

    [sr10] wired up create new signature button to redux

--- src/modules/HirezApi/components/Signature/index.js
+++ src/modules/HirezApi/components/Signature/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import Moment from 'moment'
 
 import RaisedButton from '../../../../components/RaisedButton'
 import TitledList from '../../../../components/TitledList'
 import { Container, DoubleText } from '../../../../components/ListItem'
+
+import { createSignature } from '../../actions'
 
 const Signature = props => (
   <TitledList title="Signature">
@@ -13,7 +16,17 @@ const Signature = props => (
     </Container>
 
     <Container>
-      <RaisedButton>Create new signature</RaisedButton>
+      <DoubleText label="timestamp" value={props.timestamp} />
+    </Container>
+
+    <Container>
+      <RaisedButton
+        onClick={() =>
+          props.createSignature(new Moment().utc().format('YYYYMMDDHHmmss'))
+        }
+      >
+        Create new Signature
+      </RaisedButton>
     </Container>
 
     <Container>
@@ -28,26 +41,28 @@ const Signature = props => (
       <DoubleText label="authKey" value={props.authKey} />
     </Container>
 
-    <Container>
-      <DoubleText label="timestamp" value={props.timestamp} />
-    </Container>
   </TitledList>
 )
 
 Signature.propTypes = {
   signature: PropTypes.string.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  createSignature: PropTypes.func.isRequired,
   devId: PropTypes.string.isRequired,
   method: PropTypes.string.isRequired,
-  authKey: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired
+  authKey: PropTypes.string.isRequired
 }
 
 const mapStateToProps = state => ({
   signature: state.hirezApi.signature,
+  timestamp: state.hirezApi.timestamp,
   devId: state.hirezApi.devId,
   method: state.hirezApi.method,
-  authKey: state.hirezApi.authKey,
-  timestamp: state.hirezApi.timestamp
+  authKey: state.hirezApi.authKey
 })
 
-export default connect(mapStateToProps)(Signature)
+const mapDispatchToProps = dispatch => ({
+  createSignature: timestamp => dispatch(createSignature(timestamp))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signature)
